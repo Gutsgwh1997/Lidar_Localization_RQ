a055a168178c07ad81197c3d56daae65fb9ad2b6
Gutsgwh1997 <guanwenhao55@163.com>
2020-03-02 22:59:17 +0800

    对于Tag4.0初始激光里程计做了些注释

--- include/lidar_localization/front_end/front_end.hpp
+++ include/lidar_localization/front_end/front_end.hpp
@@ -19,7 +19,7 @@
 namespace lidar_localization {
 class FrontEnd {
   public:
-    class Frame {
+    class Frame {  
       public:  
         Eigen::Matrix4f pose = Eigen::Matrix4f::Identity();
         CloudData cloud_data;
@@ -28,35 +28,47 @@ class FrontEnd {
   public:
     FrontEnd();
 
+    /**
+     * @brief 计算当前帧点云的位姿Twl
+     *
+     * @param cloud_data 当前帧点云
+     *
+     * @return 相对于第一帧点云的位姿,首帧点云的位姿通过SetInitPose()设定
+     */
     Eigen::Matrix4f Update(const CloudData& cloud_data);
     bool SetInitPose(const Eigen::Matrix4f& init_pose);
     bool SetPredictPose(const Eigen::Matrix4f& predict_pose);
 
-    bool GetNewLocalMap(CloudData::CLOUD_PTR& local_map_ptr);
+    bool GetNewLocalMap(CloudData::CLOUD_PTR& local_map_ptr);   // 这几个函数中改变了display_filter_变量，故不能是const成员函数
     bool GetNewGlobalMap(CloudData::CLOUD_PTR& global_map_ptr);
     bool GetCurrentScan(CloudData::CLOUD_PTR& current_scan_ptr);
   
   private:
+    /**
+     * @brief 更新局部地图与全局地图(全局地图的更新需要优化)
+     *
+     * @param new_key_frame 插入的新的帧
+     */
     void UpdateNewFrame(const Frame& new_key_frame);
 
   private:
-    pcl::VoxelGrid<CloudData::POINT> cloud_filter_;
-    pcl::VoxelGrid<CloudData::POINT> local_map_filter_;
-    pcl::VoxelGrid<CloudData::POINT> display_filter_;
-    pcl::NormalDistributionsTransform<CloudData::POINT, CloudData::POINT>::Ptr ndt_ptr_;
+    pcl::VoxelGrid<CloudData::POINT> cloud_filter_;               // 对新帧点云滤波
+    pcl::VoxelGrid<CloudData::POINT> local_map_filter_;           // 对于局部地图滤波
+    pcl::VoxelGrid<CloudData::POINT> display_filter_;             // GetNew...函数中，给显示用滤波
+    pcl::NormalDistributionsTransform<CloudData::POINT, CloudData::POINT>::Ptr ndt_ptr_;  // ndt匹配方法的对象
 
     std::deque<Frame> local_map_frames_;
     std::deque<Frame> global_map_frames_;
 
     bool has_new_local_map_ = false;
     bool has_new_global_map_ = false;
-    CloudData::CLOUD_PTR local_map_ptr_;
+    CloudData::CLOUD_PTR local_map_ptr_;     // 他们实际上都是智能指针，不必担心内存泄漏问题
     CloudData::CLOUD_PTR global_map_ptr_;
-    CloudData::CLOUD_PTR result_cloud_ptr_;
+    CloudData::CLOUD_PTR result_cloud_ptr_;  // 每一帧点云匹配变换后的结果
     Frame current_frame_;
 
     Eigen::Matrix4f init_pose_ = Eigen::Matrix4f::Identity();
-    Eigen::Matrix4f predict_pose_ = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f predict_pose_ = Eigen::Matrix4f::Identity();  // Tag4.0中没有使用到
 };
 }
 
--- src/front_end/front_end.cpp
+++ src/front_end/front_end.cpp
@@ -28,13 +28,14 @@ FrontEnd::FrontEnd()
 
 Eigen::Matrix4f FrontEnd::Update(const CloudData& cloud_data) {
     current_frame_.cloud_data.time = cloud_data.time;
-    std::vector<int> indices;
+    std::vector<int> indices;  // 存储移除后的点原始的索引值
     pcl::removeNaNFromPointCloud(*cloud_data.cloud_ptr, *current_frame_.cloud_data.cloud_ptr, indices);
 
     CloudData::CLOUD_PTR filtered_cloud_ptr(new CloudData::CLOUD());
     cloud_filter_.setInputCloud(current_frame_.cloud_data.cloud_ptr);
     cloud_filter_.filter(*filtered_cloud_ptr);
 
+    // 这里的静态变量很细节
     static Eigen::Matrix4f step_pose = Eigen::Matrix4f::Identity();
     static Eigen::Matrix4f last_pose = init_pose_;
     static Eigen::Matrix4f predict_pose = init_pose_;
@@ -50,10 +51,10 @@ Eigen::Matrix4f FrontEnd::Update(const CloudData& cloud_data) {
 
     // 不是第一帧，就正常匹配
     ndt_ptr_->setInputSource(filtered_cloud_ptr);
-    ndt_ptr_->align(*result_cloud_ptr_, predict_pose);
-    current_frame_.pose = ndt_ptr_->getFinalTransformation();
+    ndt_ptr_->align(*result_cloud_ptr_, predict_pose);        // 参数是变换后的点云，初始位姿估计值(这个估计值是从第一帧推来的)
+    current_frame_.pose = ndt_ptr_->getFinalTransformation(); // 原始点云到目标点云的刚体变换(由于初始估计值的设定方式，得到的是相对于第一帧的位姿)
 
-    // 更新相邻两帧的相对运动
+    // 更新相邻两帧的相对运动,使用静态变量确实很不错
     step_pose = last_pose.inverse() * current_frame_.pose;
     predict_pose = current_frame_.pose * step_pose;
     last_pose = current_frame_.pose;
@@ -81,9 +82,9 @@ bool FrontEnd::SetPredictPose(const Eigen::Matrix4f& predict_pose) {
 
 void FrontEnd::UpdateNewFrame(const Frame& new_key_frame) {
     Frame key_frame = new_key_frame;
-    // 这一步的目的是为了把关键帧的点云保存下来
+    // 这一步的目的是为了把关键帧的点云保存下来（深度拷贝）
     // 由于用的是共享指针，所以直接复制只是复制了一个指针而已
-    // 此时无论你放多少个关键帧在容器里，这些关键帧点云指针都是指向的同一个点云
+    // 此时无论你放多少个关键帧在容器里，这些关键帧点云指针都是指向的同一个点云(因为以current_frame_为参数,每次只改变*current_frame_.cloudptr)
     key_frame.cloud_data.cloud_ptr.reset(new CloudData::CLOUD(*new_key_frame.cloud_data.cloud_ptr));
     CloudData::CLOUD_PTR transformed_cloud_ptr(new CloudData::CLOUD());
     
@@ -121,8 +122,9 @@ void FrontEnd::UpdateNewFrame(const Frame& new_key_frame) {
             pcl::transformPointCloud(*global_map_frames_.at(i).cloud_data.cloud_ptr, 
                                     *transformed_cloud_ptr, 
                                     global_map_frames_.at(i).pose);
-            *global_map_ptr_ += *transformed_cloud_ptr;
+            *global_map_ptr_ += *transformed_cloud_ptr;               // 这个全局地图存在很大的问题，放在析构函数中建立地图可行吗？
         }
+        LOG(INFO)<<"There are "<<global_map_frames_.size()<<" keyframes in global map!"<<std::endl;
         has_new_global_map_ = true;
     }
 }
