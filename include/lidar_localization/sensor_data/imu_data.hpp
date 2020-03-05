260ec913799dd671e8bc44e49ce212cfc3359e52
Gutsgwh1997 <guanwenhao55@163.com>
2020-03-05 12:51:17 +0800

    新增加时间同步与四元数归一化函数

--- include/lidar_localization/sensor_data/imu_data.hpp
+++ include/lidar_localization/sensor_data/imu_data.hpp
@@ -1,5 +1,5 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: Ren Qian
  * @Date: 2019-07-17 18:27:40
  */
@@ -7,43 +7,50 @@
 #define LIDAR_LOCALIZATION_SENSOR_DATA_IMU_DATA_HPP_
 
 #include <Eigen/Dense>
+#include <deque>
 
 namespace lidar_localization {
 class IMUData {
-  public:
+   public:
     // IMU的测量值是载体在惯性空间下的加速度与角速度
     struct LinearAcceleration {
-      double x = 0.0;
-      double y = 0.0;
-      double z = 0.0;
+        double x = 0.0;
+        double y = 0.0;
+        double z = 0.0;
     };
 
     struct AngularVelocity {
-      double x = 0.0;
-      double y = 0.0;
-      double z = 0.0;
+        double x = 0.0;
+        double y = 0.0;
+        double z = 0.0;
     };
-    
-    struct Orientation {
-      double x = 0.0;
-      double y = 0.0;
-      double z = 0.0;
-      double w = 0.0;
+
+    class Orientation {
+       public:
+        double x = 0.0;
+        double y = 0.0;
+        double z = 0.0;
+        double w = 0.0;
+
+       public:
+        void Normlize() {
+            double norm = sqrt(pow(x, 2.0) + pow(y, 2.0) + pow(z, 2.0) + pow(w, 2.0));
+            x /= norm;
+            y /= norm;
+            z /= norm;
+            w /= norm;
+        }
     };
 
     double time = 0.0;
     LinearAcceleration linear_acceleration;
     AngularVelocity angular_velocity;
     Orientation orientation;
-  
-  public:
-    // 把四元数转换成旋转矩阵送出去
-    Eigen::Matrix3f GetOrientationMatrix() const {
-      Eigen::Quaterniond q(orientation.w, orientation.x, orientation.y, orientation.z);
-      Eigen::Matrix3f matrix = q.matrix().cast<float>();
 
-      return matrix;
-    }
+   public:
+    // 把四元数转换成旋转矩阵送出去
+    Eigen::Matrix3f GetOrientationMatrix() const;
+    static bool SyncData(std::deque<IMUData>& UnsyncedData, std::deque<IMUData>& SyncedData, double sync_time);
 };
 }
 #endif
